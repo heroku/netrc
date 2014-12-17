8daa00219f1282b95267e3f5443947db7ad4acf4
dickeyxxx <jeff@dickeyxxx.com>
2014-12-17 13:39:25 -0800

    v0.10.2

--- lib/netrc.rb
+++ lib/netrc.rb
@@ -1,7 +1,7 @@
 require 'rbconfig'
 
 class Netrc
-  VERSION = "0.10.1"
+  VERSION = "0.10.2"
 
   # see http://stackoverflow.com/questions/4871309/what-is-the-correct-way-to-detect-if-ruby-is-running-on-windows
   WINDOWS = RbConfig::CONFIG["host_os"] =~ /mswin|mingw|cygwin/
